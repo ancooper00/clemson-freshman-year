66550a76db469e111666a103f1e63b55d099a5f4
ancooper2000 <44234814+ancooper00@users.noreply.github.com>
2024-03-28 12:34:33 -0400

    freshmen year work

---
+++ inheritance3/inheritance3.cpp
@@ -0,0 +1,9 @@
+#include "inheritance3.h" 
+//*********************************************
+// Constructor for the Student class.         *
+//*********************************************
+Student::Student(string sname, Discipline d, Person *adv)
+                 : Person(sname) {
+    major = d;
+    advisor = adv;
+}
